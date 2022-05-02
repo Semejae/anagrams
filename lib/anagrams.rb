e6200b2d4624ccb15fe4c9f08cece59001579dc7
Anthony Gladden <a.gladden360@outlook.com>
2022-05-02 14:17:11 -0700

    created and tested method vowel_check? = pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,14 +1,14 @@
 require('pry')
 
 new_string = ''
-vowels = ['a, e, i, o, u']
+vowels = ['a, e, i, o, u, y']
 
 #1.Anagram is a word that has all the same letters of another word.
 #2.Should have the same amount of the same letters.
 #3.Needs to have a vowel.
 
 
-
+new_string = ''
 
 class Words
   attr_accessor(:string1, :string2)
@@ -16,5 +16,19 @@ class Words
     @string1 = string1
     @string2 = string2
   end
+
+  
+  def vowel_check?()
+  vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+  string_array = @string1.split(//)
+  string_array.each do |letter|
+    if vowels.include?(letter)
+      return true
+    end
+  end
+  return false
+
+  end
+
 
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -8,6 +8,14 @@ describe(Words) do
       expect(anagram.string1).to(eq('ruby'))
       expect(anagram.string2).to(eq('bury'))
     end
+  end
 
- end
+  describe('#vowel_check?') do
+    it('will return true if the string contains a vowel, else returns false') do
+      anagram = Words.new('ruby', 'bury')
+      not_words = Words.new('rb', 'br')
+      expect(anagram.vowel_check?).to(eq(true))
+      expect(not_words.vowel_check?).to(eq(false))
+    end
+  end
 end
