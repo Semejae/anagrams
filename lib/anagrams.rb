2e488219473e7e622d040154b11b8140ff9c4237
Anthony Gladden <a.gladden360@outlook.com>
2022-05-02 17:38:50 -0700

    created and tested method split = pass

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -6,6 +6,9 @@ vowels = ['a, e, i, o, u, y']
 #1.Anagram is a word that has all the same letters of another word.
 #2.Should have the same amount of the same letters.
 #3.Needs to have a vowel.
+#4. Needs to account for capilization ex. "Ruby", "Bury" .downcase/.upcase
+#5. Needs to account for spaces and punctuation. ex. "The Morse Code" is an anagram of "Here co me dots!"
+#6. Anti-gram. (no same letters)
 
 
 new_string = ''
@@ -13,22 +16,44 @@ new_string = ''
 class Words
   attr_accessor(:string1, :string2)
   def initialize(string1,string2)
-    @string1 = string1
-    @string2 = string2
+    @string1 = split(string1)
+    @string2 = split(string2)
+  end
+  
+  def split(x)
+    x.delete(' ').downcase.gsub(/[!@#$%^&*()-=+|;':",.<>?']/, '').split(//).sort
   end
 
-  
   def vowel_check?()
   vowels = ['a', 'e', 'i', 'o', 'u', 'y']
-  string_array = @string1.split(//)
-  string_array.each do |letter|
+  @string1.each do |letter|
     if vowels.include?(letter)
       return true
+      end
     end
+    return false
   end
-  return false
 
-  end
+  # def anti_gram_check?()
+  #   @string1.each do |letter|
+  #     if @string2.include?(letter)
+  #       return false
+  #     end
+  #   end
+  #   return true
+  # end
+
+  # def anagram_check?()
+  #   if vowel_check? == false
+  #     return "Is NOT an anagram."
+  #   elsif @string1.join == @string2.join
+  #     return "Is an anagram"
+  #   elsif anti_gram_check?() == true
+  #     return "is an anti-gram"
+  #   else 
+  #     return "not an anagram"
+  #   end
+  # end
 
 
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -5,8 +5,15 @@ describe(Words) do
   describe('#initialize') do 
     it('it will recognize string1 and string2 as attributes') do
       anagram = Words.new('ruby', 'bury')
-      expect(anagram.string1).to(eq('ruby'))
-      expect(anagram.string2).to(eq('bury'))
+      expect(anagram.string1).to(eq(['b','r','u','y']))
+      expect(anagram.string2).to(eq(['b','r','u','y']))
+    end
+  end
+
+  describe('#split')do
+    it ('will use a long chain of methods to change the given string into an alphabetical array with no spaces or punctuation and is lowercase') do
+      object = Words.new('Is a sentence!','ruby')
+      expect(object.string1).to(eq(["a", "c", "e", "e", "e", "i", "n", "n", "s", "s", "t"]))
     end
   end
 
@@ -18,4 +25,30 @@ describe(Words) do
       expect(not_words.vowel_check?).to(eq(false))
     end
   end
+
+  # describe('#anti_gram_check?') do 
+  #   it('will see if the given strings have no letters in common') do
+  #     anagram = Words.new('ruby', 'bury')
+  #     anti = Words.new('art', 'cow')
+  #     neither = Words.new('art', 'arc')
+  #     expect(anagram.anti_gram_check?).to(eq(false))
+  #     expect(anti.anti_gram_check?).to(eq(true))
+  #     expect(neither.anti_gram_check?).to(eq(false))
+  #   end
+  # end
+
+  # describe('#anagram_check?') do
+  #   it('will check to see if two words are anagrams') do
+  #     anagram = Words.new('ruby', 'bury')
+  #     not_anagram = Words.new('cat', 'rat')
+  #     also_not = Words.new('ctrg', 'grtc')
+  #     anti = Words.new('bed', 'rat')
+  #     expect(anagram.anagram_check?).to(eq('Is an anagram'))
+  #     expect(not_anagram.anagram_check?).to(eq('not an anagram'))
+  #     expect(also_not.anagram_check?).to(eq('Is NOT an anagram.'))
+  #     expect(anti.anagram_check?).to(eq('is an anti-gram'))
+  #   end
+
+  end
+  
 end
