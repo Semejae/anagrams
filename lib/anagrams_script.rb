11c1b0da63ee7d9bd8c8904c863a56f7549f7c02
Anthony Gladden <a.gladden360@outlook.com>
2022-05-02 17:50:37 -0700

    created scripts page

--- lib/anagrams_script.rb
+++ lib/anagrams_script.rb
@@ -1,2 +1,14 @@
 #!/usr/bin/ruby
-require_relative ''
+require_relative 'anagrams'
+
+# ./lib/anagrams_script.rb
+
+puts 'Hello'
+puts 'Enter a word'
+word1 = gets.chomp()
+puts 'Enter another word'
+word2 = gets.chomp()
+
+object = Words.new("#{word1}", "#{word2}")
+
+puts object.anagram_check?
